394969999bbae77902d202a360eca3224b9ab891
rjthornhill <rjthornhill@wpi.edu>
2015-02-17 14:04:34 -0500

    Update toTimeSync.m

--- toTimeSync.m
+++ toTimeSync.m
@@ -1,1 +1,12 @@
-toTimeSync(UEID, time_left)
+function [status] = toTimeSync(time_left)
+status = stop;
+tic ;
+time inf;
+t =0;
+while t<time
+  t = toc;
+  	if decoded
+  		time = time_left;
+  	end
+end
+status = go;
