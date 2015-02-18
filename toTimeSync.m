3db41eb9fe60ececd7a7855ec37f24d2dce48fff
rjthornhill <rjthornhill@wpi.edu>
2015-02-18 14:59:45 -0500

    Update toTimeSync.m

--- toTimeSync.m
+++ toTimeSync.m
@@ -4,7 +4,7 @@ status = stop;
 time_sec = mod(time_data, 60); %get time in seconds
 time_min = floor(time_data/60); %get time in minutes
 t = clock;
-while ((t(5) < time_min) & (t(6) < time_sec))% while the minutes and seconds are not equal to the time wanted
+while ((t(5) != time_min) & (round(t(6)) != time_sec))% while the minutes and seconds are not equal to the time wanted
 t = clock; %update the clock
 end
 [status, frameout] = decide(); %when we time out, go to decide
