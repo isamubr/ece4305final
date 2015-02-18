57b44fa8a6c63a0f9855ee6301fd95931a88414f
rjthornhill <rjthornhill@wpi.edu>
2015-02-18 15:27:15 -0500

    Update decide.m

--- decide.m
+++ decide.m
@@ -1,1 +1,12 @@
 function [status, frameout] = decide()
+[row1 col1 v1] = find(powerTable(1, 2:end));
+[row2 col2 v2] = find(powerTable(2, 2:end));
+
+avg1 = mean(v1);
+avg2 = mean(v2);
+
+if(avg1 > avg2) %BS1 is stronger
+
+elseif(avg2 > avg1) %BS2 is stronger
+
+end
