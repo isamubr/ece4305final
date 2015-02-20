0e2c516797031b26a6130ae97dd9c7f9c42fde0a
rjthornhill <rjthornhill@wpi.edu>
2015-02-20 14:47:20 -0500

    Update decide.m

--- decide.m
+++ decide.m
@@ -6,7 +6,7 @@ avg1 = mean(v1);
 avg2 = mean(v2);
 
 if(avg1 >= avg2) %BS1 is stronger
-SendReceive(FrameObj(FrameObj.REQFRAME, powerTable(1,1), UEID, 0));
+sendReceive(FrameObj(FrameObj.REQFRAME, powerTable(1,1), UEID, 0));
 elseif(avg2 > avg1) %BS2 is stronger
-SendReceive(FrameObj(FrameObj.REQFRAME, powerTable(2,1), UEID, 0));
+sendReceive(FrameObj(FrameObj.REQFRAME, powerTable(2,1), UEID, 0));
 end
