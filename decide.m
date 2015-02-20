6d9b0ef9685972ff7fbcacc1e570f4856a7b6f1b
rjthornhill <rjthornhill@wpi.edu>
2015-02-20 14:46:06 -0500

    Update decide.m

--- decide.m
+++ decide.m
@@ -1,4 +1,4 @@
-function [frameout] = decide()
+function decide()
 [row1 col1 v1] = find(powerTable(1, 2:end));
 [row2 col2 v2] = find(powerTable(2, 2:end));
 
@@ -6,7 +6,7 @@ avg1 = mean(v1);
 avg2 = mean(v2);
 
 if(avg1 >= avg2) %BS1 is stronger
-frameout = FrameObj(FrameObj.REQFRAME, powerTable(1,1), UEID, 0);
+SendReceive(FrameObj(FrameObj.REQFRAME, powerTable(1,1), UEID, 0));
 elseif(avg2 > avg1) %BS2 is stronger
-frameout = FrameObj(FrameObj.REQFRAME, powerTable(2,1), UEID, 0);
+SendReceive(FrameObj(FrameObj.REQFRAME, powerTable(2,1), UEID, 0));
 end
