59f4fcb986e762f1946e4930e7f29155585076dc
rrcooper467 <rrcooper467@gmail.com>
2015-03-02 15:51:31 -0500

    Added DataMessage
    DataMessage takes the data array of a FrameObj class variable and prints
    the original character string
    
    receivedframe=FrameObj(whatever input works for you)
    DataMessage(receivedframe.data)

---
+++ DataMessage.m
@@ -0,0 +1,17 @@
+function DataMessage( datacrc )
+%Prints the message found in the data field of the frame array
+%   the input should be _received_frame_.data  from the class FrameObj
+%   there is no output but it does print the message
+
+
+% Chop off the CRC
+databits = datacrc(1:end-8);
+% isolate the bytes
+databytes = reshape(databits',  8, []);
+% convert each byte into the decimal ascii codes
+dataascii = bi2de(databytes','left-msb');
+% convert to characters 
+char(dataascii') % no semicolon so it prints
+
+end
+
